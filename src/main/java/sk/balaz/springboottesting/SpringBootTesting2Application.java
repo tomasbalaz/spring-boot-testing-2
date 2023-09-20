db91c9f41e56b51b48f35bab00cbaae9ebd18042
Tomas Balaz <balaztomas0@gmail.com>
2023-09-20 14:49:17 +0200

    Created application spring-boot-testing-2

---
+++ src/main/java/sk/balaz/springboottesting/SpringBootTesting2Application.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboottesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTesting2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTesting2Application.class, args);
+    }
+
+}
---
+++ src/test/java/sk/balaz/springboottesting/SpringBootTesting2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTesting2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c2f9b172aa6535bbfb8af237b3c039e110697ec8
Tomas Balaz <balaztomas0@gmail.com>
2023-09-22 10:49:21 +0200

    Created customer package

---
+++ src/main/java/sk/balaz/springboottesting/customer/CustomerRegistrationService.java
@@ -0,0 +1,11 @@
+package sk.balaz.springboottesting.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerRegistrationService {
+
+    public void registerNewCustomer(CustomerRegistrationRequest request) {
+
+    }
+}

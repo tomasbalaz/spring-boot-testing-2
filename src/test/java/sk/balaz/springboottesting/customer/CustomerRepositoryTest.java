f68fff1ac0cc5ca882ce16f938e1962b179c96df
Tomas Balaz <balaztomas0@gmail.com>
2023-09-22 13:32:09 +0200

    Create CustomerRepositoryTest, add h2 DB

---
+++ src/test/java/sk/balaz/springboottesting/customer/CustomerRepositoryTest.java
@@ -0,0 +1,21 @@
+package sk.balaz.springboottesting.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository underTest;
+
+    @Test
+    void itShouldSelectCustomerByPhoneNumber() {
+        // Given
+        // When
+        // Then
+    }
+
+
+}

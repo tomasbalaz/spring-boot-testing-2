f0a6c3629467b5ba1d38d8a3dadc86ae62a2457d
Tomas Balaz <balaztomas0@gmail.com>
2023-09-22 13:57:48 +0200

    Create test itShouldSaveCustomer

--- src/test/java/sk/balaz/springboottesting/customer/CustomerRepositoryTest.java
+++ src/test/java/sk/balaz/springboottesting/customer/CustomerRepositoryTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
 class CustomerRepositoryTest {
 
@@ -17,5 +22,24 @@ class CustomerRepositoryTest {
         // Then
     }
 
+    @Test
+    void itShouldSaveCustomer() {
+        // Given - set up of something
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Abel", "00000");
 
+        // When - when something is called
+        underTest.save(customer);
+
+        // Then - assertion
+        Optional<Customer> optionalCustomer = underTest.findById(id);
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying(c -> {
+//                    assertThat(c.getId()).isEqualTo(id);
+//                    assertThat(c.getName()).isEqualTo("Abel");
+//                    assertThat(c.getPhoneNumber()).isEqualTo("111");
+                    assertThat(c).isEqualToComparingFieldByField(customer);
+                });
+    }
 }

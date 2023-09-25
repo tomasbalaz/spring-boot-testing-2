adb2412fc19dd4f80b37dffd0f37b960b41e1d37
Tomas Balaz <balaztomas0@gmail.com>
2023-09-25 16:08:58 +0200

    Create CustomerRegistrationServiceTest

---
+++ src/test/java/sk/balaz/springboottesting/customer/CustomerRegistrationServiceTest.java
@@ -0,0 +1,23 @@
+package sk.balaz.springboottesting.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class CustomerRegistrationServiceTest {
+
+     @Mock
+     private CustomerRepository customerRepository;
+    //private CustomerRepository customerRepository = mock(CustomerRepository.class);
+
+    private CustomerRegistrationService underTest;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        underTest = new CustomerRegistrationService(customerRepository);
+    }
+}

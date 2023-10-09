0304cbf3b72432eea351ffc539aab67e2abe27b4
Tomas Balaz <balaztomas0@gmail.com>
2023-10-09 15:24:53 +0200

    Updated PhoneNumberValidatorTest to use @ParameterizedTest

--- src/test/java/sk/balaz/springboottesting/utils/PhoneNumberValidatorTest.java
+++ src/test/java/sk/balaz/springboottesting/utils/PhoneNumberValidatorTest.java
@@ -2,7 +2,8 @@ package sk.balaz.springboottesting.utils;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,42 +16,19 @@ class PhoneNumberValidatorTest {
         underTest = new PhoneNumberValidator();
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "+4470000000, true",
+            "+44700000000, false",
+            "44700000000, false",
+    })
     @DisplayName("Should validate phone number")
-    void itShouldValidatePhoneNumber() {
+    void itShouldValidatePhoneNumber(String phoneNumber, boolean expected) {
         // Given
-        String phoneNumber = "+4470000000";
-
         // When
         boolean isValid = underTest.test(phoneNumber);
 
         // Then
-        assertThat(isValid).isTrue();
-    }
-
-    @Test
-    @DisplayName("Should fail when length is bigger than 13")
-    void itShouldNotValidatePhoneNumberWhenLength() {
-        // Given
-        String phoneNumber = "+44700000000";
-
-        // When
-        boolean isValid = underTest.test(phoneNumber);
-
-        // Then
-        assertThat(isValid).isFalse();
-    }
-
-    @Test
-    @DisplayName("Should fail when does not start with '+'")
-    void itShouldNotValidatePhoneNumberWhenDoesNotStart() {
-        // Given
-        String phoneNumber = "44700000000";
-
-        // When
-        boolean isValid = underTest.test(phoneNumber);
-
-        // Then
-        assertThat(isValid).isFalse();
+        assertThat(isValid).isEqualTo(expected);
     }
 }

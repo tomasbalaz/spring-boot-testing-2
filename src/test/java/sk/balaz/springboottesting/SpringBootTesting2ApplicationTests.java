86d455cdffbb69f37262cb7719e4689d47a54c63
Tomas Balaz <balaztomas0@gmail.com>
2023-10-12 14:19:56 +0200

    Add basic starting point urls

--- src/test/java/sk/balaz/springboottesting/SpringBootTesting2ApplicationTests.java
+++ src/test/java/sk/balaz/springboottesting/SpringBootTesting2ApplicationTests.java
@@ -6,6 +6,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootTesting2ApplicationTests {
 
+    // AssertJ starting point
+    //https://assertj.github.io/doc/
+
+    // Mockito starting point
+    //https://javadoc.io/doc/org.mockito/mockito-core/latest/index.html
+
+    // Power mock starting point
+    // https://github.com/powermock/powermock
     @Test
     void contextLoads() {
     }
